43318e1b19879dfb279348d403010b901e031b06
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2025-05-29 19:14:40 -0400

    idk

--- extention/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("butt").addEventListener("click", async () => {
-	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    	chrome.scripting.executeScript({
-      		target: { tabId: tabs[0].id },
-      		function: scrapeData,
-    	});
-  	});
-});
