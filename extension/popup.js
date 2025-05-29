477a3cc7a7614d5c3add022b24643ea42bbc2c56
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2025-05-29 17:22:41 -0400

    toc

---
+++ extension/popup.js
@@ -0,0 +1,31 @@
+document.getElementById("butt").addEventListener("click", async () => {
+	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    	chrome.scripting.executeScript({
+      		target: { tabId: tabs[0].id },
+      		function: scrapeData,
+    	});
+  	});
+});
+
+async function scrapeData() {
+    const title = document.title.split(" - LeetCode")[0];
+	const dateTime = document.querySelectorAll(".max-w-full.truncate")[1]?.textContent.trim();
+	const elements = document.querySelectorAll(".text-sd-foreground.text-lg.font-semibold");
+	const [runtime, runtimePercent, memory, memoryPercent] = Array.from(elements).map(el => el.textContent.trim());
+	const code = document.querySelector("code").textContent.trim();
+
+	// const capabilities = await ai.summarizer.capabilities();
+	// if (capabilities.available !== "readily") {
+	// 		console.error("no ai", capabilities);
+	// 		return;
+	// }
+	// console.log("got ai");
+
+    console.log("Title:", title);
+    console.log("Time:", dateTime);
+	console.log("Runtime:", runtime);
+	console.log("Runtime Percent:", runtimePercent);
+	console.log("Memory:", memory);
+	console.log("Memory Percent:", memoryPercent);
+	console.log("Code:", code);
+}
